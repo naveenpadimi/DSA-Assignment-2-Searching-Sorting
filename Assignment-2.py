e3bed9fe7e750f62acbb75a0955471b0ee1fc3fc
Naveen Padimi <122008488+naveenpadimi@users.noreply.github.com>
2023-03-21 11:56:11 +0530

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,84 @@
+# 1.Implement Binary Search
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+# 2.Implement Merge Sort
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+        merge_sort(left_half)
+        merge_sort(right_half)
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+
+#  3.Implement Quick Sort
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        left = []
+        right = []
+        for i in range(1, len(arr)):
+            if arr[i] < pivot:
+                left.append(arr[i])
+            else:
+                right.append(arr[i])
+        return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+# 4.Implement Insertion Sort
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+
+arr = [12, 11, 13, 5, 6]
+sorted_arr = insertion_sort(arr)
+print(sorted_arr)
+
+# 5.Write a program to sort list of strings (similar to that of dictionary)
+
+
+my_list = ["Apple", "orange", "banana", "Grape"]
+sorted_list = sorted(my_list, key=str.lower)
+print(sorted_list)
+
